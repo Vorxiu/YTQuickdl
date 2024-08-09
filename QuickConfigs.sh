6bfd8f4dfe2c3cffc077332f561332bce6bc60dc
Vorxiu <vorxiu@gmail.com>
2024-08-09 18:42:28 +0500

    Update QuickConfigs.sh

--- QuickConfigs.sh
+++ QuickConfigs.sh
@@ -86,7 +86,8 @@ download_manager() {
       "default") echo "" ;;
       "aria2c") echo "--external-downloader aria2 --external-downloader-args \"-x 16 -k 1M\"" ;;
       *) echo "" ;;
-
+   esac
+}
 # --------------------[Main script]-------------------
 echo "YT-DLP Options Configuration Script"
 
