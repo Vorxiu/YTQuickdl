5a233ba06db07a775384b5647dc057ee584afae4
Vorxiu <vorxiu@gmail.com>
2024-09-14 10:23:50 +0500

    update YTQuickDL.sh

--- YTQuickDL.sh
+++ YTQuickDL.sh
@@ -149,8 +149,8 @@ if [ -n "$playlist_title" ] && [[ "$playlist_title" != "NA" ]]; then
     download_dir="$download_dir/$playlist_title"
 else
 video_title=$(yt-dlp --get-title $URL )
-fi
 
+fi
 
 
    if [ ! -d "$download_dir" ]; then
